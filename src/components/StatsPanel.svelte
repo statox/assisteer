773021fce93787265e7f3ade92d27c36da48bd29
Adrien Fabre <adrien.fabre@dashlane.com>
2022-09-02 23:45:41 +0200

    Sort resources types in Production panel

--- src/components/StatsPanel.svelte
+++ src/components/StatsPanel.svelte
@@ -28,16 +28,46 @@ export let stats: Stats = null;
                 <td colspan=3>No object added yet...</td>
             </tr>
             {:else}
-                {#each Object.keys(stats?.resources || {}) as type }
+            {#if Object.keys(stats?.resources || {}).includes('special_resource') }
                 <tr>
+                    <td><span>Special resources</span></td>
+                    <td>
+                        {#each stats?.resources['special_resource'] as item }
+                            <div>{item.resource} x{item.count}</div>
+                        {/each}
+                    </td>
+                </tr>
+            {/if}
+            {#if Object.keys(stats?.resources || {}).includes('natural') }
+                <tr>
+                    <td><span>Natural resources</span></td>
+                    <td>
+                        {#each stats?.resources['natural'] as item }
+                            <div>{item.resource} x{item.count}</div>
+                        {/each}
+                    </td>
+                </tr>
+            {/if}
+            {#if Object.keys(stats?.resources || {}).includes('atmospheric') }
+                <tr>
+                    <td><span>Atmospheric resources</span></td>
+                    <td>
+                        {#each stats?.resources['atmospheric'] as item }
+                            <div>{item.resource} x{item.count}</div>
+                        {/each}
+                    </td>
+                </tr>
+            {/if}
+            {#each Object.keys(stats?.resources || {}).filter(type => !['special_resource', 'natural', 'atmospheric'].includes(type)) as type }
+            <tr>
                     <td><span>{type}</span></td>
                     <td>
                     {#each stats?.resources[type] as item }
                         <div>{item.resource} x{item.count}</div>
                     {/each}
-                </td>
-                </tr>
-                {/each}
+            </td>
+            </tr>
+            {/each}
             {/if}
         </table>
     </div>
