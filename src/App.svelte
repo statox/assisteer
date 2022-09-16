47394f8722b011e9600bd5f3a8be54345959a56a
Adrien Fabre <adrien.fabre@dashlane.com>
2022-09-16 15:55:59 +0200

    Add background image

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@ import ResourceDepGraph from './components/ResourceDepGraph.svelte';
 import Footer from './components/Footer.svelte';
 </script>
 
-<main>
+<main class="top-container">
     <h1 class="content-header">ASSISTEER</h1>
     <ResourceDepGraph/>
 </main>
--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,7 @@
 <main>
     <footer>
         <br/>
-        <p><i>Astroneer content and materials are the intellectual property of their respective owners.</i></p>
+        <p><i>Astroneer content and materials are the intellectual property of their respective owners</i></p>
+        <p><i>The background image comes from <a href="https://astroneer.fandom.com/wiki/Astroneer_Wiki">the astroneer wiki</a></i></p>
     </footer>
 </main>
