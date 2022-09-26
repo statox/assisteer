f6904d701839317ab810160b27a4bbd44b70ed43
Adrien Fabre <adrien.fabre@dashlane.com>
2022-09-26 20:39:59 +0200

    Move title to header

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,15 @@
 <script lang="ts">
-import About from './components/About.svelte';
-import ResourceDepGraph from './components/ResourceDepGraph.svelte';
-import Footer from './components/Footer.svelte';
-import ProjectPlanner from './components/projectPlanner/ProjectPlanner.svelte';
-
-let page='project';
+    import Header from "./components/Header.svelte";
+    import Footer from "./components/Footer.svelte";
+    import ProjectPlanner from "./components/projectPlanner/ProjectPlanner.svelte";
 </script>
 
-<style>
-    span {
-        text-transform: uppercase;
-        text-shadow: 2px 2px black;
-        font-weight: bold;
-        font-size: large;
-        margin-right: 10px;
-    }
-
-    h1 {
-        color: white;
-        text-shadow: 2px 2px black;
-        font-size: xx-large;
-    }
-</style>
+<Header />
 
 <main>
-    <h1 class="main-title">ASSISTEER</h1>
-    <p align="left">
-        <span on:click={() => {page='project'}}>Project planner</span>
-        <span on:click={() => {page='about'}}>About</span>
-    </p>
-    <div class="top-container">
-        {#if page === 'project'}
-            <ResourceDepGraph/>
-        {:else}
-            <About/>
-        {/if}
-    </div>
-    <br/>
     <div>
-        <ProjectPlanner/>
+        <ProjectPlanner />
     </div>
 </main>
 
-<Footer/>
+<Footer />
---
+++ src/components/Header.svelte
@@ -0,0 +1,13 @@
+<main>
+    <header>
+        <h1 class="main-title">ASSISTEER</h1>
+    </header>
+</main>
+
+<style>
+    h1 {
+        color: white;
+        text-shadow: 2px 2px black;
+        font-size: xx-large;
+    }
+</style>
