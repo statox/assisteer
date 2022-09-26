17b3bbf831a441b64c9a2fd652cdde574b7dc0bf
Adrien Fabre <adrien.fabre@dashlane.com>
2022-09-27 00:11:42 +0200

    Rework Resource list UI

--- src/components/projectPlanner/ProjectResourcesList.svelte
+++ src/components/projectPlanner/ProjectResourcesList.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { afterUpdate } from "svelte";
+    import { getObject } from "../../services/data/objects";
     import {
         getProjectResourcesList,
         Project,
@@ -7,26 +8,77 @@
     } from "../../services/project";
     export let project: Project;
 
+    const favoriteCategoriesOrder = {
+        "special_resource": 1,
+        atmospheric: 2,
+        natural: 3,
+        refined: 4,
+        composite: 5,
+    };
     let resourcesList: ResourceList = {};
+    let sortedCategories = [];
+
+    const alphaSort = (a: string, b: string) => a < b ? -1 : 1;
 
     afterUpdate(() => {
         if (!project) {
             return;
         }
         resourcesList = getProjectResourcesList(project);
+
+        sortedCategories = Object.keys(resourcesList).sort((a, b) => {
+            const favOrderA = favoriteCategoriesOrder[a];
+            const favOrderB = favoriteCategoriesOrder[b];
+
+            // Simple alphabetical sort
+            if (!favOrderA && !favOrderB) {
+                return a < b ? -1 : 1;
+            }
+
+            if (!favOrderA) {
+                return 1;
+            }
+
+            if (!favOrderB) {
+                return -1;
+            }
+
+            return favOrderA - favOrderB;
+        });
     });
 </script>
 
 <main>
     <div>
         <h3 class="content-header">Resource list</h3>
-        {#each Object.keys(resourcesList) as category}
+        {#each sortedCategories as category}
             <div>
-                <h4>{category}</h4>
-                {#each Object.keys(resourcesList[category]) as objectName}
-                    <p>{objectName} x{resourcesList[category][objectName]}</p>
-                {/each}
+                <h4 class="content-subheader">{category}</h4>
+                <ul>
+                    {#each Object.keys(resourcesList[category]).sort(alphaSort) as objectName}
+                        <li>
+                            <span>
+                                <b>{resourcesList[category][objectName]}</b>
+                                &nbsp;{objectName}
+                                &nbsp;<img
+                                    class="resource-icon"
+                                    src={getObject(objectName).url.image}
+                                    alt={getObject(objectName).labels.en}
+                                />
+                            </span>
+                        </li>
+                    {/each}
+                </ul>
             </div>
         {/each}
     </div>
 </main>
+
+<style>
+    ul {
+        list-style-type: none;
+    }
+    .resource-icon {
+        width: 3em;
+    }
+</style>
