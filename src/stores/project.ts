19204732657e50104e277f322761666f7794fd65
Adrien Fabre <adrien.fabre@dashlane.com>
2022-09-28 00:22:36 +0200

    Store project in local storage

--- src/stores/project.ts
+++ src/stores/project.ts
@@ -1,6 +1,23 @@
-import { writable } from "svelte/store";
+import { Writable, writable } from "svelte/store";
 import type { Project } from "../services/project";
 
-const project = writable<Project>({});
+
+const storedProjectStr = localStorage.getItem("project");
+
+let project: Writable<Project>;
+if (storedProjectStr !== null) {
+    try {
+        project = writable<Project>(JSON.parse(storedProjectStr) as Project);
+    } catch (e) {
+        console.error('Couldnt load project from local storage');
+        project = writable<Project>({});
+    }
+} else {
+    project = writable<Project>({});
+}
+
+project.subscribe((value) => {
+    localStorage.setItem("project", JSON.stringify(value));
+});
 
 export { project };
