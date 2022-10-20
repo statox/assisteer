4ac07fe4591fbdbf22f4561aeaed05b08f2a9e7c
Adrien Fabre <adrien.fabre@dashlane.com>
2022-10-21 01:05:06 +0200

    Minor UI tweaks in ProjectObjectsList

--- src/components/projectPlanner/inventory/ProjectObjectsList.svelte
+++ src/components/projectPlanner/inventory/ProjectObjectsList.svelte
@@ -68,73 +68,76 @@
                             {projectTotalUnlockCost}&nbsp;bytes
                         </div>
                     </div>
+
+                    <div class="col-sm-4 d-flex justify-content-between">
+                        <span class="important-word">Sort items by&nbsp</span>
+                        <div>
+                            <div class="form-check form-check-inline">
+                                <input
+                                    checked={sortType === 'category'}
+                                    on:click={() => changeSortType('category')}
+                                    class="form-check-input"
+                                    type="radio"
+                                    id="pictureImage"
+                                    value="image"
+                                />
+                                <label class="form-check-label" for="pictureImage">category</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input
+                                    checked={sortType === 'tier'}
+                                    on:click={() => changeSortType('tier')}
+                                    class="form-check-input"
+                                    type="radio"
+                                    id="pictureIcon"
+                                    value="icon"
+                                />
+                                <label class="form-check-label" for="pictureIcon">tier</label>
+                            </div>
+                        </div>
+                    </div>
+
                     <button class="col-sm-4 btn-danger" on:click={() => changeQuantity({ op: 'reset' })}
                         >Reset project</button
                     >
-
-                    <div class="col-sm-4">
-                        <span class="important-word">Sort items by&nbsp</span>
-                        <div class="form-check form-check-inline">
-                            <input
-                                checked={sortType === 'category'}
-                                on:click={() => changeSortType('category')}
-                                class="form-check-input"
-                                type="radio"
-                                id="pictureImage"
-                                value="image"
-                            />
-                            <label class="form-check-label" for="pictureImage">category</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input
-                                checked={sortType === 'tier'}
-                                on:click={() => changeSortType('tier')}
-                                class="form-check-input"
-                                type="radio"
-                                id="pictureIcon"
-                                value="icon"
-                            />
-                            <label class="form-check-label" for="pictureIcon">tier</label>
-                        </div>
-                    </div>
                 </div>
 
                 {#each Object.keys(objectsByCategory).sort(alphaSort) as category}
                     <h4 class="content-subheader">{category}</h4>
                     {#each objectsByCategory[category] as item}
                         <div class="row align-items-center bottom-separator">
-                            <div class="col-sm-4">
+                            <div class="col-sm-8 d-flex justify-content-between">
                                 <ObjectName
                                     pictureType="icon"
                                     object={item.object}
                                     importantWord={true}
                                     largerIcon={true}
                                 />
-                            </div>
-                            <div class="col-sm-4 text-align-center">
-                                <button
-                                    on:click={() =>
-                                        changeQuantity({
-                                            objectName: item.objectName,
-                                            op: 'dec'
-                                        })}>-</button
-                                >
-                                &nbsp{item.quantity}&nbsp
-                                <button
-                                    on:click={() =>
-                                        changeQuantity({
-                                            objectName: item.objectName,
-                                            op: 'inc'
-                                        })}>+</button
-                                >
-                                <button
-                                    class="btn-danger"
-                                    on:click={() =>
-                                        changeQuantity({
-                                            objectName: item.objectName,
-                                            op: 'remove'
-                                        })}>Remove</button
-                                >
+                                <div class="text-align-center">
+                                    <button
+                                        on:click={() =>
+                                            changeQuantity({
+                                                objectName: item.objectName,
+                                                op: 'dec'
+                                            })}>-</button
+                                    >
+                                    &nbsp{item.quantity}&nbsp
+                                    <button
+                                        on:click={() =>
+                                            changeQuantity({
+                                                objectName: item.objectName,
+                                                op: 'inc'
+                                            })}>+</button
+                                    >
+                                    <button
+                                        class="btn-danger"
+                                        on:click={() =>
+                                            changeQuantity({
+                                                objectName: item.objectName,
+                                                op: 'remove'
+                                            })}>Remove</button
+                                    >
+                                </div>
                             </div>
                             <div class="col-sm-4 text-align-center">
                                 <span>
