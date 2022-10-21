3e3f3b7f9b6259ac8c4e244b7d69ce001214a0c1
Adrien Fabre <adrien.fabre@dashlane.com>
2022-10-21 13:34:44 +0200

    Order planet by distance to the sun

--- src/services/data/planets.ts
+++ src/services/data/planets.ts
@@ -13,14 +13,18 @@ export type Planet = {
     id: string;
     type: string;
     resources?: LocationDetails;
+    moonId?: string;
     gases: {
         [gasId: string]: number;
     };
-    difficulty: 'easy' | 'medium' | 'hard' | 'very hard';
+    // difficulty: 'easy' | 'medium' | 'hard' | 'very hard';
+    difficulty: string;
     power: {
         dayNightCycleSeconds: number;
-        sun: 'very low' | 'low' | 'medium' | 'high' | 'very high';
-        wind: 'very low' | 'low' | 'medium' | 'high' | 'very high';
+        // sun: 'very low' | 'low' | 'medium' | 'high' | 'very high';
+        // wind: 'very low' | 'low' | 'medium' | 'high' | 'very high';
+        sun: string;
+        wind: string;
         wikiWindCoefficient: number; // The wiki says the wind coeffients are not related to the planets wind power category
     };
     gatewayNetwork: {
@@ -36,18 +40,19 @@ export type Planet = {
     labels: {
         [lang: string]: string;
     };
+    orderToSun: number;
 };
 
 const getPlanet = (planetId: string): Planet => {
     return planets[planetId];
 };
 
-const allPlanets = [];
+const allPlanets: Planet[] = [];
 for (const planet of Object.values(planets)) {
-    allPlanets.push(planet as Planet);
+    allPlanets.push(planet);
 }
 const getAllPlanets = (): Planet[] => {
-    return allPlanets;
+    return allPlanets.sort((a, b) => a.orderToSun - b.orderToSun);
 };
 
 const getAllPlanetsMap = () => planets;
