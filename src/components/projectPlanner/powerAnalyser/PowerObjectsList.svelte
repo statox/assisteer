6625450f6dce9563b8e3b2617148478e32a43274
Adrien Fabre <adrien.fabre@dashlane.com>
2022-11-02 22:28:21 +0100

    Extract PowerObjectsList to its own component

---
+++ src/components/projectPlanner/powerAnalyser/PowerObjectsList.svelte
@@ -0,0 +1,154 @@
+<script lang="ts">
+    import type { ProjectPowerStats } from '../../../services/data/power';
+    import { updateObjectQuantityInProject } from '../../../services/project';
+
+    import { objectSelectionModalShow, project } from '../../../stores';
+    import ObjectName from '../../utils/ObjectName.svelte';
+    import QuantitySelector from '../inventory/QuantitySelector.svelte';
+
+    export let projectData: ProjectPowerStats;
+</script>
+
+<div class="table-responsive">
+    <div>
+        <button
+            class="nav-item"
+            on:click={() => {
+                $objectSelectionModalShow = true;
+            }}
+            data-toggle="tooltip"
+            data-placement="top"
+            title="Object selection"
+        >
+            <span class="bi bi-card-list" />
+        </button>
+    </div>
+
+    <table class="table table-borderless">
+        <tbody>
+            {#if projectData['producer'].total > 0}
+                <tr>
+                    <td colspan="4"><h4 class="content-subheader">Power Production</h4></td>
+                </tr>
+                <tr>
+                    <td />
+                    <td class="small-header">By object</td>
+                    <td class="small-header">By type</td>
+                    <td />
+                </tr>
+                {#each projectData['producer'].items as item}
+                    <tr>
+                        <td>
+                            <ObjectName object={item.object} pictureType="icon" importantWord={true} />
+                        </td>
+                        <!-- this is here for typeguard -->
+                        {#if item.powerStats.type === 'producer'}
+                            <td>
+                                {item.quantity} x {item.powerStats.output} U/s
+                            </td>
+                            <td>
+                                {item.quantity * item.powerStats.output} U/s
+                            </td>
+                        {/if}
+                        <td>
+                            <QuantitySelector
+                                objectId={item.object.id}
+                                quantity={$project.objects[item.object.id]}
+                                changeQuantityFn={(params) =>
+                                    ($project = updateObjectQuantityInProject($project, {
+                                        op: params.op,
+                                        objectId: params.objectName
+                                    }))}
+                            />
+                        </td>
+                    </tr>
+                {/each}
+            {/if}
+            {#if projectData['storage'].totalCapacity > 0}
+                <tr>
+                    <td colspan="4"><h4 class="content-subheader">Power Storage</h4></td>
+                </tr>
+                <tr>
+                    <td />
+                    <td class="small-header">By object</td>
+                    <td class="small-header">By type</td>
+                    <td />
+                </tr>
+                {#each projectData['storage'].items as item}
+                    <tr>
+                        <td>
+                            <ObjectName object={item.object} pictureType="icon" importantWord={true} />
+                        </td>
+                        <!-- this is here for typeguard -->
+                        {#if item.powerStats.type === 'storage'}
+                            <td>
+                                {item.quantity} x {item.powerStats.capacity} U
+                            </td>
+                            <td>
+                                {item.quantity * item.powerStats.capacity} U
+                            </td>
+                        {/if}
+                        <td>
+                            <QuantitySelector
+                                objectId={item.object.id}
+                                quantity={$project.objects[item.object.id]}
+                                changeQuantityFn={(params) =>
+                                    ($project = updateObjectQuantityInProject($project, {
+                                        op: params.op,
+                                        objectId: params.objectName
+                                    }))}
+                            />
+                        </td>
+                    </tr>
+                {/each}
+            {/if}
+            {#if projectData['consumer'].total > 0}
+                <tr>
+                    <td colspan="4"><h4 class="content-subheader">Power Consumption</h4></td>
+                </tr>
+                <tr>
+                    <td />
+                    <td class="small-header">By object</td>
+                    <td class="small-header">By type</td>
+                    <td />
+                </tr>
+                {#each projectData['consumer'].items as item}
+                    <tr>
+                        <td>
+                            <ObjectName object={item.object} pictureType="icon" importantWord={true} />
+                        </td>
+                        <!-- this is here for typeguard -->
+                        {#if item.powerStats.type === 'consumer'}
+                            <td>
+                                {item.quantity} x {item.powerStats.input} U/s
+                            </td>
+                            <td>
+                                {item.quantity * item.powerStats.input} U/s
+                            </td>
+                        {/if}
+                        <td>
+                            <QuantitySelector
+                                objectId={item.object.id}
+                                quantity={$project.objects[item.object.id]}
+                                changeQuantityFn={(params) =>
+                                    ($project = updateObjectQuantityInProject($project, {
+                                        op: params.op,
+                                        objectId: params.objectName
+                                    }))}
+                            />
+                        </td>
+                    </tr>
+                {/each}
+            {/if}
+        </tbody>
+    </table>
+</div>
+
+<style>
+    .table-borderless tr {
+        border: 0;
+    }
+    .small-header {
+        font-size: 0.7em;
+    }
+</style>
--- src/components/projectPlanner/powerAnalyser/ProjectPowerAnalyser.svelte
+++ src/components/projectPlanner/powerAnalyser/ProjectPowerAnalyser.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
     import { getProjectPowerStats, ProjectPowerStats } from '../../../services/data/power';
-    import { objectSelectionModalShow, project } from '../../../stores';
+    import { project } from '../../../stores';
     import { selectedPowerPlanet } from '../../../stores/selectedPowerPlanet';
-    import ObjectName from '../../utils/ObjectName.svelte';
     import PlanetSelection from './PlanetSelection.svelte';
-    import QuantitySelector from '../inventory/QuantitySelector.svelte';
-    import { updateObjectQuantityInProject } from '../../../services/project';
     import ProjectPowerTimeline from './ProjectPowerTimeline.svelte';
+    import PowerObjectsList from './PowerObjectsList.svelte';
 
     let collapsed = false;
     let hasDataToShow = false;
@@ -142,139 +140,9 @@
                         </div>
                     </div>
                 {/if}
-                <div class="row table-responsive">
-                    <div>
-                        <button
-                            class="nav-item"
-                            on:click={() => {
-                                $objectSelectionModalShow = true;
-                            }}
-                            data-toggle="tooltip"
-                            data-placement="top"
-                            title="Object selection"
-                        >
-                            <span class="bi bi-card-list" />
-                        </button>
-                    </div>
 
-                    <table class="table table-borderless">
-                        <tbody>
-                            {#if projectData['producer'].total > 0}
-                                <tr>
-                                    <td colspan="4"><h4 class="content-subheader">Power Production</h4></td>
-                                </tr>
-                                <tr>
-                                    <td />
-                                    <td class="small-header">By object</td>
-                                    <td class="small-header">By type</td>
-                                    <td />
-                                </tr>
-                                {#each projectData['producer'].items as item}
-                                    <tr>
-                                        <td>
-                                            <ObjectName object={item.object} pictureType="icon" importantWord={true} />
-                                        </td>
-                                        <!-- this is here for typeguard -->
-                                        {#if item.powerStats.type === 'producer'}
-                                            <td>
-                                                {item.quantity} x {item.powerStats.output} U/s
-                                            </td>
-                                            <td>
-                                                {item.quantity * item.powerStats.output} U/s
-                                            </td>
-                                        {/if}
-                                        <td>
-                                            <QuantitySelector
-                                                objectId={item.object.id}
-                                                quantity={$project.objects[item.object.id]}
-                                                changeQuantityFn={(params) =>
-                                                    ($project = updateObjectQuantityInProject($project, {
-                                                        op: params.op,
-                                                        objectId: params.objectName
-                                                    }))}
-                                            />
-                                        </td>
-                                    </tr>
-                                {/each}
-                            {/if}
-                            {#if projectData['storage'].totalCapacity > 0}
-                                <tr>
-                                    <td colspan="4"><h4 class="content-subheader">Power Storage</h4></td>
-                                </tr>
-                                <tr>
-                                    <td />
-                                    <td class="small-header">By object</td>
-                                    <td class="small-header">By type</td>
-                                    <td />
-                                </tr>
-                                {#each projectData['storage'].items as item}
-                                    <tr>
-                                        <td>
-                                            <ObjectName object={item.object} pictureType="icon" importantWord={true} />
-                                        </td>
-                                        <!-- this is here for typeguard -->
-                                        {#if item.powerStats.type === 'storage'}
-                                            <td>
-                                                {item.quantity} x {item.powerStats.capacity} U
-                                            </td>
-                                            <td>
-                                                {item.quantity * item.powerStats.capacity} U
-                                            </td>
-                                        {/if}
-                                        <td>
-                                            <QuantitySelector
-                                                objectId={item.object.id}
-                                                quantity={$project.objects[item.object.id]}
-                                                changeQuantityFn={(params) =>
-                                                    ($project = updateObjectQuantityInProject($project, {
-                                                        op: params.op,
-                                                        objectId: params.objectName
-                                                    }))}
-                                            />
-                                        </td>
-                                    </tr>
-                                {/each}
-                            {/if}
-                            {#if projectData['consumer'].total > 0}
-                                <tr>
-                                    <td colspan="4"><h4 class="content-subheader">Power Consumption</h4></td>
-                                </tr>
-                                <tr>
-                                    <td />
-                                    <td class="small-header">By object</td>
-                                    <td class="small-header">By type</td>
-                                    <td />
-                                </tr>
-                                {#each projectData['consumer'].items as item}
-                                    <tr>
-                                        <td>
-                                            <ObjectName object={item.object} pictureType="icon" importantWord={true} />
-                                        </td>
-                                        <!-- this is here for typeguard -->
-                                        {#if item.powerStats.type === 'consumer'}
-                                            <td>
-                                                {item.quantity} x {item.powerStats.input} U/s
-                                            </td>
-                                            <td>
-                                                {item.quantity * item.powerStats.input} U/s
-                                            </td>
-                                        {/if}
-                                        <td>
-                                            <QuantitySelector
-                                                objectId={item.object.id}
-                                                quantity={$project.objects[item.object.id]}
-                                                changeQuantityFn={(params) =>
-                                                    ($project = updateObjectQuantityInProject($project, {
-                                                        op: params.op,
-                                                        objectId: params.objectName
-                                                    }))}
-                                            />
-                                        </td>
-                                    </tr>
-                                {/each}
-                            {/if}
-                        </tbody>
-                    </table>
+                <div class="row">
+                    <PowerObjectsList {projectData} />
                 </div>
             {/if}
         </div>
@@ -282,9 +150,6 @@
 </main>
 
 <style>
-    .table-borderless tr {
-        border: 0;
-    }
     .red {
         color: var(--red);
     }
@@ -293,8 +158,5 @@
     }
     .small-text {
         font-size: 0.9em;
-    }
-    .small-header {
-        font-size: 0.7em;
     }
 </style>
